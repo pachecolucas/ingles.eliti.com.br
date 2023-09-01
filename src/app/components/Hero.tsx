2753744e5f72d09c4758925f0514fa2b61559782
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2023-09-01 13:25:13 -0300

    conteúdo seco

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,157 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col gap-5 py-10 px-5 text-slate-700">
+      <h1 className="font-bold uppercase text-xl text-center text-slate-900">
+        Desafio <span className="text-[2em]">3</span> da ELITI
+      </h1>
+
+      <p className="text-center uppercase text-sm font-bold text-slate-900">
+        A linguagem MAIS IMPORTANTE que um profissional de verdade da área de
+        tecnologia deve saber não é HTML, CSS ou Javascript.{" "}
+        <span className="font-extrabold">É inglês</span>!
+      </p>
+
+      <p>
+        Lembra que você disse ter interesse em aprender inglês o mais rápido
+        possível sem desculpas ou timidez? É exatamente sobre isso este desafio!
+      </p>
+
+      <p>
+        Apesar de ser matéria obrigatória na escola, apenas 1% dos brasileiros
+        possuem fluência neste idioma. Isso é uma triste realidade que fecha
+        portas no mundo inteiro para profissionais brasileiros.
+      </p>
+
+      <p>
+        Mas você pode estar se perguntando porque agora vai aprender inglês de
+        verdade enquanto na escola tradicional provavelmente só ficou estagnado
+        no tal verbo To Be por anos sem resultados.
+      </p>
+
+      <p>
+        Na ELITI não temos aulas e nem provas de inglês. Isso porque entendemos,
+        assim como o educador José Pacheco, que aula não ensina e prova não
+        avalia. Então não adiante perder tempo com isso.
+      </p>
+
+      <p>
+        Lembra da rotina de se preparar para uma prova no dia anterior, tirar
+        uma nota suficiente para passar e depois de 6 meses ter esquecido tudo?
+        A ELITI não funciona assim!
+      </p>
+
+      <p>
+        O foco aqui é na sua fluência, não na sua capacidade de decorar regras
+        inúteis para fazer provas e esquecer tudo depois.
+      </p>
+
+      <p>
+        Por isso, preparamos com carinho e muita pesquisa um material simples e,
+        ao mesmo tempo, muito poderoso capaz de ajudar você a finalmente ganhar
+        fluência em inglês.
+      </p>
+
+      <p>E agora a melhor notícia:</p>
+      <p>VOCÊ NÃO ESTÁ SOZINHO</p>
+
+      <p>
+        Nós queremos te ajudar a destravar seu inglês e passar de uma vez neste
+        desafio. Por isso, a partir de agora, você já pode continuar conversando
+        com um professor da ELITI para tirar dúvidas sobre este desafio no
+        WhatApp abaixo sempre que precisar.
+      </p>
+
+      <p># O que devo saber de inglês para entrar na ELITI</p>
+
+      <p>
+        Você deve saber quem é A.J.Hoge! A gente ainda vai dar boas rizadas com
+        memes desse cara porque não existe elitiano de verdade que não sonhe com
+        a voz deste gringo falando inglês na sua cabeça o tempo todo.
+      </p>
+
+      <p>
+        É sério! A.J.Hoge é um professor de inglês espetacular. Seu método de
+        ensino começou a chamar atenção quando suas turminas de 5º ou 6º ano no
+        lugar que ficarem repetindo o verbo To Be começaram a ficar fluentes em
+        inglês de verdade surpreendendo a todos.
+      </p>
+
+      <p>
+        E o que ele faz de tão diferente? Ele ensina o inglês real e não o
+        inglês da escolinha. Para isso, A.J.Hoge desenvolveu o que ele chamou
+        de:
+      </p>
+
+      <p>7 RULES TO LEARN REAL ENGLISH</p>
+      <p>(7 regras para aprender inglês real)</p>
+
+      <p>
+        E, destas 7 regras, a única que você precisa saber agora para entrar na
+        ELITI é a regra número 1:
+      </p>
+
+      <p>LEARN ENGLISH PHRASES, NOT INDIVIDUAL WORDS</p>
+      <p>(Aprenda frases em inglês, não palavras individuais)</p>
+
+      <p>
+        Além de poder focar agora apenas nesta primeira regra, sem precisar se
+        preocupar com as outras 6, temos uma outra boa notícia:
+      </p>
+
+      <p>
+        Tudo que você precisa saber sobre a primeira regra está contido no 3
+        primeiros parágrafos da transcrição do vídeo abaixo, ou seja, apenas os
+        x primeiros minutos deste vídeo do A.J.Hoge:
+      </p>
+
+      <p>[VÍDEO]</p>
+
+      <p>
+        Abaixo temos a transcrição dos 3 primeiros parágrafos que ele falou. Sua
+        primeira missão é conseguir entender 100% do que está escrito aqui em
+        inglês. Use a internet a seu favor para te ajudar a alcançar este
+        primeiro objetivo:
+      </p>
+
+      <p>
+        Conseguiu entender tudo? Muito bem! Agora é hora de enviar uma mensagem
+        de voz para o Whatapp abaixo onde você deve tentar ler apenas o primeiro
+        parágrafo abaixo onde A.J.Hoge diz quem ele é e dá as boas vindas ao seu
+        curso em vídeo:
+      </p>
+
+      <p>
+        Hi! I'm A.J.Hoge, one of the teachers of Learn Real English. And welcome
+        to our video course.
+      </p>
+
+      <p>
+        Sua missão agora é ouvir o vídeo quantas vezes forem necessárias,
+        entender tudo que ele esta falando e conseguir então enviar um áudio
+        para o Whatsapp abaixo imitando da forma mais parecida possível ao
+        A.J.Hoge esta primeira frase acima que ele falou.
+      </p>
+
+      <p>
+        Conseguindo fazer isso, você fará posteriormente o mesmo com o segundo e
+        terceiro parágrafos também. Mas calma! Não precisa enviar todos de uma
+        vez só agora.
+      </p>
+
+      <p>
+        Envie um primeiro áudio imitando apenas o que A.J.Hoge diz no primeiro
+        parágrafo acima. O professor da ELITI que estará em contato com você
+        pelo whatsapp vai avisar quando for o momento de enviar os demais.
+      </p>
+
+      <p>[BTN: ENVIAR AÚDIO DO PRIMEIRO PARÁGRAFO]</p>
+
+      <p>
+        Apesar de que não ser necessário enviar agora (caso seja a primeira vez
+        que você esta entrando por aqui) vamos deixar aqui também registrado a
+        transcrições dos dois parágrafos que virão a seguir apenas para
+        facilitar interações futuras e ter tudo já organizado neste site:
+      </p>
+    </div>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ELITI - Desafio Inglês",
+  description: "Desafio de inglês para entrar na ELITI",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
